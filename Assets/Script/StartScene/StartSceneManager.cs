0fe92904836db5f9351ea3293e621d543d5e8f61
sw9332 <117564386+sw9332@users.noreply.github.com>
2024-06-04 17:53:57 +0900

    StartSceneManager.cs 업데이트
    주석 추가

--- Assets/Script/StartScene/StartSceneManager.cs
+++ Assets/Script/StartScene/StartSceneManager.cs
@@ -15,6 +15,8 @@ public class StartSceneManager : MonoBehaviour
 
     public static int Screen_Frame;
 
+    //Screen.currentReolution.refreshRate 현재 모니터 주사율을 가져옴으로써 개인 컴퓨터의 최적화 된 프레임으로 동기화 가능.
+
     void Awake()
     {
         Screen_Frame = Screen.currentResolution.refreshRate;
