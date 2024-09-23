8814c7d8ce904d86d2f47f03a0cd03f1d8335c2a
SW9332 <117564386+sw9332@users.noreply.github.com>
2024-09-23 19:47:58 +0900

    Inventory 코드 개선 - 1

--- Assets/Script/GameScene/Player/Inventory.cs
+++ Assets/Script/GameScene/Player/Inventory.cs
@@ -5,11 +5,9 @@ using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour
 {
-    // 슬롯 배열
-    public string[] item_main_slot = new string[4];
-    public Image[] item_main_slot_Image = new Image[4];
+    public string[] itemDB = new string[4];
+    public Image[] itemImageDB = new Image[4];
 
-    // 아이템 오브젝트 및 스프라이트
     public GameObject BrownTeddyBear_Object;
     public GameObject PinkTeddyBear_Object;
     public GameObject YellowTeddyBear_Object;
@@ -29,73 +27,17 @@ public class Inventory : MonoBehaviour
     [SerializeField]
     public Dialogue d_camera;
 
-    // 아이템 두기 / 올려두기
-    public void Slot1()
+    // 아이템 배치
+    public void PlaceItem(int slotIndex)
     {
-        if (Player.object_collision == "땅")
-        {
-            Instantiate(GetItemObject(item_main_slot[0]), Player.pos, Quaternion.identity);
-            item_main_slot[0] = null;
-            item_main_slot_Image[0].sprite = null;
-        }
+        if (itemDB[slotIndex] == null) return;
 
-        if (Player.object_collision == "사물")
-        {
-            Instantiate(GetItemObject(item_main_slot[0]), Object.pos, Quaternion.identity);
-            item_main_slot[0] = null;
-            item_main_slot_Image[0].sprite = null;
-        }
-    }
+        GameObject itemObject = GetItemObject(itemDB[slotIndex]);
+        Vector3 position = Player.object_collision == "땅" ? Player.pos : Object.pos;
 
-    public void Slot2()
-    {
-        if (Player.object_collision == "땅")
-        {
-            Instantiate(GetItemObject(item_main_slot[1]), Player.pos, Quaternion.identity);
-            item_main_slot[1] = null;
-            item_main_slot_Image[1].sprite = null;
-        }
-
-        if (Player.object_collision == "사물")
-        {
-            Instantiate(GetItemObject(item_main_slot[1]), Object.pos, Quaternion.identity);
-            item_main_slot[1] = null;
-            item_main_slot_Image[1].sprite = null;
-        }
-    }
-
-    public void Slot3()
-    {
-        if (Player.object_collision == "땅")
-        {
-            Instantiate(GetItemObject(item_main_slot[2]), Player.pos, Quaternion.identity);
-            item_main_slot[2] = null;
-            item_main_slot_Image[2].sprite = null;
-        }
-
-        if (Player.object_collision == "사물")
-        {
-            Instantiate(GetItemObject(item_main_slot[2]), Object.pos, Quaternion.identity);
-            item_main_slot[2] = null;
-            item_main_slot_Image[2].sprite = null;
-        }
-    }
-
-    public void Slot4()
-    {
-        if (Player.object_collision == "땅")
-        {
-            Instantiate(GetItemObject(item_main_slot[3]), Player.pos, Quaternion.identity);
-            item_main_slot[3] = null;
-            item_main_slot_Image[3].sprite = null;
-        }
-
-        if (Player.object_collision == "사물")
-        {
-            Instantiate(GetItemObject(item_main_slot[3]), Object.pos, Quaternion.identity);
-            item_main_slot[3] = null;
-            item_main_slot_Image[3].sprite = null;
-        }
+        Instantiate(itemObject, position, Quaternion.identity);
+        itemDB[slotIndex] = null;
+        itemImageDB[slotIndex].sprite = null;
     }
 
     // 아이템 줍기 
@@ -135,13 +77,14 @@ public class Inventory : MonoBehaviour
     {
         if (other.gameObject.tag == "DroppedBrownTeddyBear" && Input.GetKeyDown(KeyCode.Space))
         {
-            for (int i = 0; i < item_main_slot.Length; i++)
+            for (int i = 0; i < itemDB.Length; i++)
             {
-                if (item_main_slot[i] == "" || item_main_slot[i] == null)
+                if (itemDB[i] == "" || itemDB[i] == null)
                 {
-                    item_main_slot[i] = "BrownTeddyBear";
-                    item_main_slot_Image[i].sprite = GetItemSprite(item_main_slot[i]);
+                    itemDB[i] = "BrownTeddyBear";
+                    itemImageDB[i].sprite = GetItemSprite(itemDB[i]);
                     Destroy(other.gameObject);
+
                     dialogueManager.ShowDialogue(d_camera);
                     uiManager.CameraUI.SetActive(true);
                     break;
@@ -149,21 +92,14 @@ public class Inventory : MonoBehaviour
             }
         }
 
-        if (other.gameObject.tag == "Camera" && Input.GetKeyDown(KeyCode.Space))
-        {
-            dialogueManager.ShowDialogue(d_camera);
-            uiManager.CameraUI.SetActive(true);
-            Destroy(other.gameObject);
-        }
-
         if (other.gameObject.tag == "BrownTeddyBear" && Input.GetKeyDown(KeyCode.Space))
         {
-            for (int i = 0; i < item_main_slot.Length; i++)
+            for (int i = 0; i < itemDB.Length; i++)
             {
-                if (item_main_slot[i] == "" || item_main_slot[i] == null)
+                if (itemDB[i] == "" || itemDB[i] == null)
                 {
-                    item_main_slot[i] = "BrownTeddyBear";
-                    item_main_slot_Image[i].sprite = GetItemSprite(item_main_slot[i]);
+                    itemDB[i] = "BrownTeddyBear";
+                    itemImageDB[i].sprite = GetItemSprite(itemDB[i]);
                     Destroy(other.gameObject);
                     break;
                 }
@@ -172,12 +108,12 @@ public class Inventory : MonoBehaviour
 
         if (other.gameObject.tag == "PinkTeddyBear" && Input.GetKeyDown(KeyCode.Space))
         {
-            for (int i = 0; i < item_main_slot.Length; i++)
+            for (int i = 0; i < itemDB.Length; i++)
             {
-                if (item_main_slot[i] == "" || item_main_slot[i] == null)
+                if (itemDB[i] == "" || itemDB[i] == null)
                 {
-                    item_main_slot[i] = "PinkTeddyBear";
-                    item_main_slot_Image[i].sprite = GetItemSprite(item_main_slot[i]);
+                    itemDB[i] = "PinkTeddyBear";
+                    itemImageDB[i].sprite = GetItemSprite(itemDB[i]);
                     Destroy(other.gameObject);
                     break;
                 }
@@ -186,12 +122,12 @@ public class Inventory : MonoBehaviour
 
         if (other.gameObject.tag == "YellowTeddyBear" && Input.GetKeyDown(KeyCode.Space))
         {
-            for (int i = 0; i < item_main_slot.Length; i++)
+            for (int i = 0; i < itemDB.Length; i++)
             {
-                if (item_main_slot[i] == "" || item_main_slot[i] == null)
+                if (itemDB[i] == "" || itemDB[i] == null)
                 {
-                    item_main_slot[i] = "YellowTeddyBear";
-                    item_main_slot_Image[i].sprite = GetItemSprite(item_main_slot[i]);
+                    itemDB[i] = "YellowTeddyBear";
+                    itemImageDB[i].sprite = GetItemSprite(itemDB[i]);
                     Destroy(other.gameObject);
                     break;
                 }
@@ -200,12 +136,12 @@ public class Inventory : MonoBehaviour
 
         if (other.gameObject.tag == "Cake" && Input.GetKeyDown(KeyCode.Space))
         {
-            for (int i = 0; i < item_main_slot.Length; i++)
+            for (int i = 0; i < itemDB.Length; i++)
             {
-                if (item_main_slot[i] == "" || item_main_slot[i] == null)
+                if (itemDB[i] == "" || itemDB[i] == null)
                 {
-                    item_main_slot[i] = "Cake";
-                    item_main_slot_Image[i].sprite = GetItemSprite(item_main_slot[i]);
+                    itemDB[i] = "Cake";
+                    itemImageDB[i].sprite = GetItemSprite(itemDB[i]);
                     Destroy(other.gameObject);
                     break;
                 }
@@ -214,12 +150,12 @@ public class Inventory : MonoBehaviour
 
         if (other.gameObject.tag == "NPC" && Input.GetKeyDown(KeyCode.Space))
         {
-            for (int i = 0; i < item_main_slot.Length; i++)
+            for (int i = 0; i < itemDB.Length; i++)
             {
-                if (item_main_slot[i] == "" || item_main_slot[i] == null)
+                if (itemDB[i] == "" || itemDB[i] == null)
                 {
-                    item_main_slot[i] = "NPC";
-                    item_main_slot_Image[i].sprite = GetItemSprite(item_main_slot[i]);
+                    itemDB[i] = "NPC";
+                    itemImageDB[i].sprite = GetItemSprite(itemDB[i]);
                     Destroy(other.gameObject);
                     break;
                 }
@@ -235,24 +171,9 @@ public class Inventory : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            Slot1();
-        }
-
-        if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            Slot2();
-        }
-
-        if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            Slot3();
-        }
-
-        if (Input.GetKeyDown(KeyCode.Alpha4))
-        {
-            Slot4();
-        }
+        if (Input.GetKeyDown(KeyCode.Alpha1)) PlaceItem(0);
+        if (Input.GetKeyDown(KeyCode.Alpha2)) PlaceItem(1);
+        if (Input.GetKeyDown(KeyCode.Alpha3)) PlaceItem(2);
+        if (Input.GetKeyDown(KeyCode.Alpha4)) PlaceItem(3);
     }
 }
