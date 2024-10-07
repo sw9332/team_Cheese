792e8bd067d7aa18a501ef9ccf49d76b75f7d6be
dae-won-kim <2000daewon@naver.com>
2024-10-07 20:41:20 +0900

    playerDamaged
    - player가 피격당했을 시의 애니메이션 추가
    - 스프라이트 단일만으로는 애니메이션 도중 이미지 교체가 되지 않아서 애니메이션으로 따로 만들어서 0.3초동안 못움직이도록 설정

--- Assets/Script/GameScene/Player/PlayerControl.cs
+++ Assets/Script/GameScene/Player/PlayerControl.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
     private Player player;
+    private PlayerAttack playerAttack;
     private Stamina stamina;
 
-    public SpriteRenderer rend; // player 스프라이트 (바라보는 방향 설정)
+   // public SpriteRenderer rend; // player 스프라이트 (바라보는 방향 설정)
     public Animator animator; // player 이동 및 공격 애니메이션
 
     public static float MoveSpeed = 2.5f;
@@ -49,7 +50,7 @@ public class PlayerControl : MonoBehaviour
     {
         animator.speed = 1;
 
-        if (!isMove) animator.Play("PlayerBack_Stop");
+        if (!isMove && !playerAttack.isChangingSprite) animator.Play("PlayerBack_Stop");
 
         if (isMove)
         {
@@ -165,6 +166,7 @@ public class PlayerControl : MonoBehaviour
     {
         player = FindObjectOfType<Player>();
         stamina = FindObjectOfType<Stamina>();
+        playerAttack = FindObjectOfType<PlayerAttack>();
     }
 
     void Update()
