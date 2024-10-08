0ee3f0cfa7afd43a664d5de29d6e4857f1d4bdbb
SW9332 <117564386+sw9332@users.noreply.github.com>
2024-10-08 17:15:01 +0900

    Demo2

--- Assets/Script/Camera/MiniGame.cs
+++ Assets/Script/Camera/MiniGame.cs
@@ -13,7 +13,7 @@ public class MiniGame : MonoBehaviour
     public GameObject minigamePanel;
     public GameObject ingameUIPanel;
     public GameObject player;
-    
+
     // E키 카메라앨범 이미지에 어떤 이미지가 들어가야 할 지 판단하는 변수들
     public static bool isImageChange = false;
 
@@ -47,46 +47,36 @@ public class MiniGame : MonoBehaviour
             ingameUIPanel.SetActive(false);
             minigamePanel.SetActive(true);
             is_minigame = true;
+
+            Debug.Log("Slider X : " + x_Axis.value + ", " + "Slider Y : " + y_Axis.value);
         }
     }
 
     private void TakePhoto()
     {
-        //GameState가 Tutorial일 때.
-        if (GameManager.GameState == "Tutorial")
+        if (is_minigame)
         {
-            if (is_minigame == true)
+            if (Input.GetKey(KeyCode.LeftArrow)) photoCamera.transform.Translate(-1f * 1.5f * Time.deltaTime, 0f, 0f);
+            if (Input.GetKey(KeyCode.RightArrow)) photoCamera.transform.Translate(1f * 1.5f * Time.deltaTime, 0f, 0f);
+            if (Input.GetKey(KeyCode.UpArrow)) photoCamera.transform.Translate(0f, 1f * 1.5f * Time.deltaTime, 0f);
+            if (Input.GetKey(KeyCode.DownArrow)) photoCamera.transform.Translate(0f, -1f * 1.5f * Time.deltaTime, 0f);
+
+            //GameState가 Tutorial일 때.
+            if (GameManager.GameState == "Tutorial")
             {
-                if (Input.GetKey(KeyCode.LeftArrow))
-                {
-                    photoCamera.transform.Translate(-1f * 1.5f * Time.deltaTime, 0f, 0f);
-                }
-                if (Input.GetKey(KeyCode.RightArrow))
-                {
-                    photoCamera.transform.Translate(1f * 1.5f * Time.deltaTime, 0f, 0f);
-                }
-                if (Input.GetKey(KeyCode.UpArrow))
-                {
-                    photoCamera.transform.Translate(0f, 1f * 1.5f * Time.deltaTime, 0f);
-                }
-                if (Input.GetKey(KeyCode.DownArrow))
-                {
-                    photoCamera.transform.Translate(0f, -1f * 1.5f * Time.deltaTime, 0f);
-                }
+                float ClampX = Mathf.Clamp(photoCamera.transform.position.x, -78.2f, -76.2f);
+                float ClampY = Mathf.Clamp(photoCamera.transform.position.y, 48f, 50f);
 
-                float ClampX = Mathf.Clamp(photoCamera.transform.GetComponent<Transform>().position.x, -78.2f, -76.2f);
-                float ClampY = Mathf.Clamp(photoCamera.transform.GetComponent<Transform>().position.y, 48f, 50f);
+                photoCamera.transform.position = new Vector3(ClampX, ClampY, -1f);
 
-                photoCamera.transform.GetComponent<Transform>().position = new Vector3(ClampX, ClampY, -1f);
-
-                x_Axis.GetComponent<Slider>().value = photoCamera.transform.position.x;
-                y_Axis.GetComponent<Slider>().value = photoCamera.transform.position.y;
+                x_Axis.value = photoCamera.transform.position.x;
+                y_Axis.value = photoCamera.transform.position.y;
 
                 if (Input.GetKey(KeyCode.F)
-                    && x_Axis.GetComponent<Slider>().value <= -76.9f && x_Axis.GetComponent<Slider>().value >= -77.1f
-                    && y_Axis.GetComponent<Slider>().value <= 48.7f && y_Axis.GetComponent<Slider>().value >= 48.35f)
+                    && x_Axis.value <= -76.9f && x_Axis.value >= -77.1f
+                    && y_Axis.value <= 48.7f && y_Axis.value >= 48.35f)
                 {
-                    //is_next_stage = true;
+                    // 다음 스테이지로 넘어가는 로직
                     UIManager.is_cake = false;
                     UIManager.is_bear = false;
                     UIManager.tutorialTrigger = false;
@@ -99,54 +89,36 @@ public class MiniGame : MonoBehaviour
                 }
             }
 
-            //if (is_next_stage && !is_transition)
-            //{
-
-            //}
-        }
-
-        //GameState가 Stage1일 때.
-        else if (GameManager.GameState == "Stage1")
-        {
-            if (is_minigame == true)
+            //GameState가 Stage1일 때.
+            if (GameManager.GameState == "Stage1")
             {
-                if (Input.GetKey(KeyCode.LeftArrow))
-                {
-                    photoCamera.transform.Translate(-1f * 1.5f * Time.deltaTime, 0f, 0f);
-                }
-                if (Input.GetKey(KeyCode.RightArrow))
-                {
-                    photoCamera.transform.Translate(1f * 1.5f * Time.deltaTime, 0f, 0f);
-                }
-                if (Input.GetKey(KeyCode.UpArrow))
-                {
-                    photoCamera.transform.Translate(0f, 1f * 1.5f * Time.deltaTime, 0f);
-                }
-                if (Input.GetKey(KeyCode.DownArrow))
-                {
-                    photoCamera.transform.Translate(0f, -1f * 1.5f * Time.deltaTime, 0f);
-                }
+                x_Axis.minValue = 11.8f;
+                x_Axis.maxValue = 13.8f;
 
-                float ClampX = Mathf.Clamp(photoCamera.transform.GetComponent<Transform>().position.x, 11.8f, 12.8f);
-                float ClampY = Mathf.Clamp(photoCamera.transform.GetComponent<Transform>().position.y, 29.5f, 30.5f);
+                y_Axis.minValue = 29.5f;
+                y_Axis.maxValue = 31.5f;
 
-                photoCamera.transform.GetComponent<Transform>().position = new Vector3(ClampX, ClampY, -1f);
+                float ClampX = Mathf.Clamp(photoCamera.transform.position.x, 11.8f, 13.8f);
+                float ClampY = Mathf.Clamp(photoCamera.transform.position.y, 29.5f, 31.5f);
 
-                x_Axis.GetComponent<Slider>().value = photoCamera.transform.position.x;
-                y_Axis.GetComponent<Slider>().value = photoCamera.transform.position.y;
-                Debug.Log("Slider X: " + x_Axis.value + " | Slider Y: " + y_Axis.value);
+                photoCamera.transform.position = new Vector3(ClampX, ClampY, -1f);
+
+                x_Axis.value = photoCamera.transform.position.x;
+                y_Axis.value = photoCamera.transform.position.y;
 
                 if (Input.GetKey(KeyCode.F)
-                    && x_Axis.GetComponent<Slider>().value <= -12.4f && x_Axis.GetComponent<Slider>().value >= -12.6f
-                    && y_Axis.GetComponent<Slider>().value <= 29.9f && y_Axis.GetComponent<Slider>().value >= 30.1f)
+                    && x_Axis.value <= 12.8f && x_Axis.value >= 13f
+                    && y_Axis.value <= 29.8f && y_Axis.value >= 30.2f)
                 {
                     is_take_photo = false;
                     is_minigame = false;
                     isImageChange = true;
+                    Debug.Log("데모 끝");
                 }
             }
         }
     }
+
 
     void CutSceneText()
     {
