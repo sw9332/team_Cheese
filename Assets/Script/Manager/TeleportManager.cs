88dd2d011a33effacb23af05bc6ead850515b2ac
SW9332 <117564386+sw9332@users.noreply.github.com>
2024-10-11 11:27:57 +0900

    TeleportManager.cs

--- Assets/Script/Manager/TeleportManager.cs
+++ Assets/Script/Manager/TeleportManager.cs
@@ -5,9 +5,8 @@ using UnityEngine.UI;
 
 public class TeleportManager : MonoBehaviour
 {
-    public string locationName;
     public Text MapNameText;
-    public float fadeDuration = 1f;
+    private float fadeDuration = 1f;
     private Coroutine fadeCoroutine;
 
     public void teleport(string locationName, Collider2D other)
@@ -15,53 +14,34 @@ public class TeleportManager : MonoBehaviour
         switch (locationName)
         {
             case "파티룸 (입구)": GameManager.GameState = "파티룸";
-                other.transform.position = new Vector3(57.5f, -1.8f, 0);
-                ShowMapNameText(GameManager.GameState);
-                break;
+                other.transform.position = new Vector3(57.5f, -1.8f, 0); ShowMapNameText(GameManager.GameState); break;
             case "파티룸 (출구)": GameManager.GameState = "복도 #F";
-                other.transform.position = new Vector3(other.transform.position.x, -11.3f, 0);
-                ShowMapNameText(GameManager.GameState);
-                break;
+                other.transform.position = new Vector3(other.transform.position.x, -11.3f, 0); ShowMapNameText(GameManager.GameState); break;
 
             case "연회장 입구 (입구)": GameManager.GameState = "연회장 입구";
-                other.transform.position = new Vector3(other.transform.position.x, 7.5f, 0f);
-                ShowMapNameText(GameManager.GameState);
-                break;
+                other.transform.position = new Vector3(other.transform.position.x, 7.5f, 0f); ShowMapNameText(GameManager.GameState); break;
             case "연회장 입구 (출구)": GameManager.GameState = "복도 #F";
-                other.transform.position = new Vector3(other.transform.position.x, -2.33f, 0f);
-                ShowMapNameText(GameManager.GameState);
-                break;
+                other.transform.position = new Vector3(other.transform.position.x, -2.33f, 0f); ShowMapNameText(GameManager.GameState); break;
 
             case "연회장 (입구)": GameManager.GameState = "연회장";
-                other.transform.position = new Vector3(59f, 19.67f, 0f);
-                ShowMapNameText(GameManager.GameState);
-                break;
+                other.transform.position = new Vector3(59f, 19.67f, 0f); ShowMapNameText(GameManager.GameState); break;
             case "연회장 (출구)": GameManager.GameState = "연회장 입구";
-                other.transform.position = new Vector3(46.61f, 19.67f, 0f);
-                ShowMapNameText(GameManager.GameState);
-                break;
+                other.transform.position = new Vector3(46.61f, 19.67f, 0f); ShowMapNameText(GameManager.GameState); break;
 
             case "창고 입구 (입구)": GameManager.GameState = "창고 입구";
-                other.transform.position = new Vector3(other.transform.position.x, -1.7f, 0f);
-                ShowMapNameText(GameManager.GameState);
-                break;
+                other.transform.position = new Vector3(other.transform.position.x, -1.7f, 0f); ShowMapNameText(GameManager.GameState); break;
             case "창고 입구 (출구)": GameManager.GameState = "복도 #F";
-                other.transform.position = new Vector3(other.transform.position.x, -11.6f, 0f);
-                ShowMapNameText(GameManager.GameState);
-                break;
+                other.transform.position = new Vector3(other.transform.position.x, -11.6f, 0f); ShowMapNameText(GameManager.GameState); break;
 
             case "창고 (입구)": GameManager.GameState = "창고";
-                other.transform.position = new Vector3(other.transform.position.x, 18.5f, 0f);
-                ShowMapNameText(GameManager.GameState);
-                break;
+                other.transform.position = new Vector3(other.transform.position.x, 18.5f, 0f); ShowMapNameText(GameManager.GameState); break;
             case "창고 (출구)": GameManager.GameState = "창고 입구";
-                other.transform.position = new Vector3(other.transform.position.x, 8.6f, 0f);
-                ShowMapNameText(GameManager.GameState);
-                break;
+                other.transform.position = new Vector3(other.transform.position.x, 8.6f, 0f); ShowMapNameText(GameManager.GameState); break;
 
             case "RoomE Go": GameManager.GameState = "-";
                 other.transform.position = new Vector3(27.76f, -49.45f, 0f); ShowMapNameText(GameManager.GameState); break;
-            case "RoomE Exit": other.transform.position = new Vector3(41.15f, -42.31f, 0f); ShowMapNameText(GameManager.GameState); break;
+            case "RoomE Exit": GameManager.GameState = "-";
+                other.transform.position = new Vector3(41.15f, -42.31f, 0f); ShowMapNameText(GameManager.GameState); break;
 
             case "RoomF Go": GameManager.GameState = "-";
                 other.transform.position = new Vector3(40.97f, -58.25f, 0f); ShowMapNameText(GameManager.GameState); break;
