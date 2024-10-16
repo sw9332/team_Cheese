3328ac5728b71e4af252f3aeb3052bf82f7cf930
SW9332 <117564386+sw9332@users.noreply.github.com>
2024-10-17 00:27:50 +0900

    FadeManager, DialogueManager
    - Fade중에 대화창으로 인해 isMove가 켜지는 것과, 대화창이 실행 중일 때 FadeIn으로 인해 isMove가 켜지지 않도록 변경

--- Assets/Script/Camera/CameraManager.cs
+++ Assets/Script/Camera/CameraManager.cs
@@ -5,20 +5,20 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
-    public Transform player;
+    private Player player;
 
     public Vector3 pos;
     private Vector3 velocity = Vector3.zero;
 
-    void LateUpdate()
+    void FixedUpdate()
     {
         pos = transform.position;
 
         switch (GameManager.GameState)
         {
             case "Æ©Åä¸®¾ó":
-                if(player.position.y >= 47 && player.position.y <= 50)
-                    pos.y = player.position.y;
+                if(player.transform.position.y >= 47 && player.transform.position.y <= 50)
+                    pos.y = player.transform.position.y;
                 break;
 
             case "Æ©Åä¸®¾ó ÄÆ¾À":
@@ -28,38 +28,51 @@ public class CameraManager : MonoBehaviour
 
             case "ÆÄÆ¼·ë":
                 pos.x = 60f;
-                pos.y = player.position.y;
+                pos.y = player.transform.position.y;
                 break;
 
             case "º¹µµ #F":
-                if (player.position.y >= -7.5f)
+                if (player.transform.position.y >= -7.5f)
                 {
-                    pos.x = player.position.x;
-                    pos.y = player.position.y;
+                    pos.x = player.transform.position.x;
+                    pos.y = player.transform.position.y;
                 }
                     
                 else
                 {
-                    pos.x = player.position.x;
+                    pos.x = player.transform.position.x;
                     pos.y = -12.5f;
                 }
                 break;
 
             case "¿¬È¸Àå ÀÔ±¸":
                 pos.x = 44f;
-                pos.y = player.position.y;
+                pos.y = player.transform.position.y;
+                break;
+
+            case "¿¬È¸Àå":
+                bool posX = (player.transform.position.x >= 58.3f && player.transform.position.x <= 73.8f);
+                bool posY = (player.transform.position.y <= 19.5f && player.transform.position.y >= 15f);
+                if (posX) pos.x = player.transform.position.x;
+                if (posY) pos.y = player.transform.position.y;
+                if (!posX && !posY) pos = transform.position;
                 break;
 
             case "Ã¢°í":
-                pos.y = player.position.y;
+                pos.y = player.transform.position.y;
                 break;
 
             default:
-                pos.x = player.position.x;
-                pos.y = player.position.y;
+                pos.x = player.transform.position.x;
+                pos.y = player.transform.position.y;
                 break;
         }
 
         transform.position = pos;
+    }
+
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
     }
 }
