98a3ea16660babc25afa2a2c11e8bde34edac139
SW9332 <117564386+sw9332@users.noreply.github.com>
2024-11-02 20:21:11 +0900

    Table Item
    - 테이블에 케이크만 설치 가능

--- Assets/Script/Object/Table.cs
+++ Assets/Script/Object/Table.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Rendering;
 
@@ -7,12 +8,47 @@ public class Table : MonoBehaviour
 {
     public GameObject CamaraEvent;
 
+    private DialogueManager dialogueManager;
+    private DialogueContentManager dialogueContentManager;
+    private Inventory inventory;
+
+    private bool isCake = false;
+
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("Cake")) //케이크를 테이블에 놓았을때 생기는 이벤트 오브젝트
+        if (dialogueManager.dialogue_continue) return;
+
+        var TeddyBearItem = other.gameObject.GetComponent<TeddyBearItem>();
+
+        if (TeddyBearItem != null && TeddyBearItem.isInstalled) return;
+
+        if (other.gameObject.CompareTag("Cake") && !isCake) //케이크를 테이블에 놓았을때 생기는 이벤트 오브젝트
         {
             CamaraEvent.SetActive(true);
             UIManager.is_cake = true;
+            isCake = true;
         }
+
+        else if (other.CompareTag("BrownTeddyBear") || other.CompareTag("PinkTeddyBear") || other.CompareTag("YellowTeddyBear"))
+        {
+            dialogueManager.ShowDialogue(dialogueContentManager.d_not_a_cake);
+
+            for (int i = 0; i < inventory.SlotDB.Length; i++)
+                if (inventory.SlotDB[i] == null)
+                {
+                    inventory.SlotDB[i] = other.tag;
+                    inventory.SlotImageDB[i].sprite = inventory.GetItemSprite(other.tag);
+                    TeddyBearItem.isInstalled = true;
+                    Destroy(other.gameObject);
+                    break;
+                }
+        }
+    }
+
+    void Start()
+    {
+        dialogueManager = FindObjectOfType<DialogueManager>();
+        dialogueContentManager = FindObjectOfType<DialogueContentManager>();
+        inventory = FindObjectOfType<Inventory>();
     }
 }
