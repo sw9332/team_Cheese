8cf14d701dbd3058c6fb1f244811c8f3bcd74991
SW9332 <117564386+sw9332@users.noreply.github.com>
2025-01-13 02:35:49 +0900

    Update Stamina.cs

--- Assets/Script/UI/Stamina.cs
+++ Assets/Script/UI/Stamina.cs
@@ -8,29 +8,6 @@ public class Stamina : MonoBehaviour
     public Slider playerStaminaBar;
     public bool isPlayerRunning;
 
-    void staminaChage()
-    {
-        if (isPlayerRunning && !PlayerControl.isPush)
-        {
-            staminaDown();
-        }
-            
-        else
-        {
-            staminaUp();
-        }
-    }
-
-    void staminaDown()
-    {
-        playerStaminaBar.value -= 0.5f * Time.deltaTime;
-    }
-
-    void staminaUp()
-    {
-        playerStaminaBar.value += 0.25f * Time.deltaTime;
-    }
-
     void Start()
     {
         playerStaminaBar.value = 1f;
@@ -38,6 +15,7 @@ public class Stamina : MonoBehaviour
 
     void Update()
     {
-        staminaChage(); 
+        if (isPlayerRunning && !PlayerControl.isPush) playerStaminaBar.value -= 0.5f * Time.deltaTime;
+        else playerStaminaBar.value += 0.25f * Time.deltaTime;
     }
 }
