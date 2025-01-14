16fd811202ae9e4e76bcb49c9210aa264d693c59
dae-won-kim <2000daewon@naver.com>
2025-01-14 23:56:07 +0900

    bear moving
    Bear moves when player is close to it.

--- Assets/Script/Enemy/Enemy.cs
+++ Assets/Script/Enemy/Enemy.cs
@@ -2,13 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
 
 public class Enemy : MonoBehaviour
 {
     public int hp = 3;
+    public Transform playerTransform;
 
+
+    [SerializeField] Vector2 playerCheckBox;
+    private Collider2D player;
+    private SpriteRenderer spriteRenderer;
+    private string enemyName;
     private GameObject enemy;
-    [SerializeField] Animator enemyEffects;  // Animator 컴포넌트
+    private float moveSpeed;
+    private Rigidbody2D rb;
+
+    [SerializeField] Animator animator;
 
     public void destroyEnemy()
     {
@@ -28,35 +39,87 @@ public class Enemy : MonoBehaviour
     {
         if ((enemy.name + "Die") != null)
         {
-            enemyEffects.Play(enemy.name + "Die");
-            yield return new WaitForSeconds(enemyEffects.GetCurrentAnimatorStateInfo(0).length);
+            animator.Play(enemy.name + "Die");
+            yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
         }
         // 애니메이션 재생 후 오브젝트 삭제
         destroyEnemy();
     }
     public void bearIdle()
     {
-        string enemyName = enemy.name; // 오브젝트 이름 기반
         if (gameObject.layer == LayerMask.NameToLayer("enemy"))
         {
-            // 전환 중이 아니고 상태가 Idle이 아니면 Idle 상태로 전환
-            if (!enemyEffects.IsInTransition(0) && !enemyEffects.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+            if (!animator.IsInTransition(0) && !animator.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
             {
-                // Debug.Log($"Switching to Idle for {enemyName}");
-                enemyEffects.Play(this.gameObject.name + "Idle");
+                animator.Play(this.gameObject.name + "Idle");
             }
         }
+        else
+            return;
     }
 
+    public bool showRangeGizmo = false;
+    private void OnDrawGizmosSelected()
+    {
+        if (showRangeGizmo)
+        {
+            Gizmos.color = new Color(1.0f, 0f, 0f, 0.8f);
+            Gizmos.DrawCube(this.transform.position, new Vector2(playerCheckBox.x, playerCheckBox.y));
+        }
 
+    }
+    public bool isNearPlayer()
+    {
+        // check player
+        Collider2D[] colliders = Physics2D.OverlapBoxAll(transform.position, playerCheckBox, 0f);
+        foreach (Collider2D collider in colliders)
+        {
+            if (collider.gameObject.tag == "Player")
+            {
+                player = collider; 
+                return true;
+            }
+        }
+        return false;
+    }
 
     public void bearMove()
     {
-        string enemyName = enemy.name;
         if (gameObject.layer == LayerMask.NameToLayer("enemy"))
         {
-            enemyEffects.Play(enemyName + "Walk");
+            if (isNearPlayer()) 
+            {
+                // direction vector
+                Vector2 direction = (playerTransform.position - this.transform.position).normalized;
+
+                // enemy is on player's left
+                if (playerTransform.position.x > this.transform.position.x)
+                {
+                    spriteRenderer.flipX = false;
+                    spriteRenderer.flipY = false;
+                }
+                else // enemy is on player's right
+                {
+                    spriteRenderer.flipX = false;
+                    spriteRenderer.flipY = true;
+                }
+                // move position 
+                rb.MovePosition((Vector2)transform.position + direction * moveSpeed * Time.fixedDeltaTime);
+
+                // Enemy rotate
+                float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+                rb.rotation = angle;
+
+                animator.Play(enemyName + "Walk");
+                Debug.Log($"Bear is walking towards {player.name}");
+            }
+            else 
+            {
+                bearIdle();
+            }
         }
+        else
+            return;
     }
 
     void Update()
@@ -67,16 +130,20 @@ public class Enemy : MonoBehaviour
         }
         else
         {
-            // Idle 상태를 지속적으로 유지
-            bearIdle();
+            bearMove();
         }
-
-       // Debug.Log("Current Animator State: " + enemyEffects.GetCurrentAnimatorStateInfo(0).IsName(this.name + "Idle"));
     }
+
 
     void Start()
     {
         enemy = this.gameObject;
-        enemyEffects = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        playerCheckBox.x = 8.0f;
+        playerCheckBox.y = 3.0f;
+        moveSpeed = 2.0f;
+        enemyName = enemy.name;
+        animator = GetComponent<Animator>();
     }
 }
