63ddb1327a946a4302282f3dd6ee4ab1cd24a7d8
SW9332 <117564386+sw9332@users.noreply.github.com>
2025-02-04 03:49:08 +0900

    Update N_Player.cs

--- Assets/N_Player.cs
+++ Assets/N_Player.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class N_Player : MonoBehaviour
 {
-    private PlayerControl player;
     private DialogueManager dialogueManager;
     private DialogueContentManager dialogueContentManager;
 
@@ -12,55 +11,6 @@ public class N_Player : MonoBehaviour
 
     public bool isFollow = false;
     public bool event4 = false;
-
-    public float followDistance = 1f;
-
-    void FollowPlayer()
-    {
-        if (isFollow)
-        {
-            Vector3 directionToPlayer = player.transform.position - transform.position;
-            float distanceToPlayer = directionToPlayer.magnitude;
-            directionToPlayer.Normalize();
-
-            if (distanceToPlayer < followDistance)
-            {
-                animator.Play("N_Player");
-                return;
-            }
-
-            if (Mathf.Abs(directionToPlayer.x) > Mathf.Abs(directionToPlayer.y))
-            {
-                if (!player.stop)
-                {
-                    if (directionToPlayer.x > 0)
-                        animator.Play("N_Player Right");
-                    else
-                        animator.Play("N_Player Left");
-                }
-
-                else animator.Play("N_Player");
-
-            }
-
-            else
-            {
-                if (!player.stop)
-                {
-                    if (directionToPlayer.y > 0)
-                        animator.Play("N_Player Up");
-                    else
-                        animator.Play("N_Player Down");
-                }
-
-                else animator.Play("N_Player");
-            }
-
-            transform.position = player.transform.position - directionToPlayer * followDistance;
-        }
-
-        else animator.Play("N_Player");
-    }
 
     void OnTriggerEnter2D(Collider2D other)
     {
@@ -91,20 +41,9 @@ public class N_Player : MonoBehaviour
         }
     }
 
-    void Update()
-    {
-        FollowPlayer();
-
-        if (Input.GetKeyDown(KeyCode.Q) && !isFollow) isFollow = true;
-        else if (Input.GetKeyDown(KeyCode.Q) && isFollow) isFollow = false;
-    }
-
     void Start()
     {
-        player = FindFirstObjectByType<PlayerControl>();
         dialogueManager = FindFirstObjectByType<DialogueManager>();
         dialogueContentManager = FindFirstObjectByType<DialogueContentManager>();
-
-        animator = GetComponent<Animator>();
     }
 }
