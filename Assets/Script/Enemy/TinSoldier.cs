4fd08a665c550464baff304a70d85c4447993119
SW9332 <117564386+sw9332@users.noreply.github.com>
2025-02-08 05:27:23 +0900

    Update TinSoldier.cs

--- Assets/Script/Enemy/TinSoldier.cs
+++ Assets/Script/Enemy/TinSoldier.cs
@@ -5,4 +5,29 @@ using UnityEngine;
 public class TinSoldier : MonoBehaviour
 {
     public float speed = 1;
+
+    private Animator animator;
+
+    public void Move(string direction)
+    {
+        animator.Play(direction);
+
+        switch (direction)
+        {
+            case "Up": transform.Translate(Vector2.up * speed * Time.deltaTime); break;
+            case "Down": transform.Translate(Vector2.down * speed * Time.deltaTime); break;
+            case "Left": transform.Translate(Vector2.left * speed * Time.deltaTime); break;
+            case "Right": transform.Translate(Vector2.right * speed * Time.deltaTime); break;
+        }
+    }
+
+    public void Stop(string direction)
+    {
+        animator.Play(direction+" Stop");
+    }
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
 }
