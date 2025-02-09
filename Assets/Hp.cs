c609f3d6e08c83c3fffa4fe4a3b7d4cb4b2e535d
SW9332 <117564386+sw9332@users.noreply.github.com>
2025-02-10 00:05:53 +0900

    Hp 스크립트 추가

---
+++ Assets/Hp.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hp : MonoBehaviour
+{
+    public GameObject[] hpObject;
+    public float hpValue = 10;
+
+    private static GameObject value = null;
+
+    public static GameObject Instance
+    {
+        get
+        {
+            return value;
+        }
+    }
+
+    void Awake()
+    {
+        if (value == null)
+            value = this.gameObject;
+        else
+            Destroy(value);
+    }
+
+    void HpUpdate()
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            hpObject[i].SetActive(i < hpValue);
+        }
+    }
+
+    public void HpPlus(float value)
+    {
+        hpValue += value;
+        HpUpdate();
+    }
+
+    public void HpDecrease(float value)
+    {
+        hpValue -= value;
+        HpUpdate();
+    }
+}
