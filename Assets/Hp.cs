a86aaf8f73f102e4438b9409ca9e2a7ed38442e7
SW9332 <117564386+sw9332@users.noreply.github.com>
2025-02-11 14:50:51 +0900

    Update Hp.cs

--- Assets/Hp.cs
+++ Assets/Hp.cs
@@ -58,7 +58,7 @@ public class Hp : MonoBehaviour
 
     IEnumerator WaitForAnimation()
     {
-        yield return new WaitForSeconds(0.2f);
+        yield return new WaitForSeconds(0.3f);
         HpUpdate();
     }
 
